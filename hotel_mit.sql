2c0c7398c3d41d6761eb7fc21d137b4baacaea6c
vantuzfed <ivan238777@gmail.com>
2024-11-29 01:04:08 +0300

    new hotel bd

--- hotel_mit.sql
+++ hotel_mit.sql
@@ -1,48 +1,62 @@
 drop schema if exists hotel;
 create schema if not exists hotel;
 use hotel;
-
-create table rooms(
-id int primary key,
-class varchar(20),
-price int
+CREATE TABLE `bill` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `book_id` int NOT NULL unique,
+  `emp_id` int NOT NULL,
+  `client_id` int NOT NULL,
+  `price_total` int DEFAULT null
 );
 
-create table clients(
-id int primary key auto_increment,
-first_name varchar(20),
-middle_name varchar(20),
-last_name varchar(20)
+CREATE TABLE `booking` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `room_id` int NOT NULL unique,
+  `book_start` timestamp NOT NULL,
+  `book_end` timestamp NOT NULL,
+  `book_status` varchar(20) DEFAULT 'Начало'
 );
 
-create table booking(
-id int primary key auto_increment,
-room_id int,
-client_id int,
-foreign key (room_id) references rooms(id),
-foreign key (client_id) references clients(id)
+CREATE TABLE `clients` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(20) NOT NULL,
+  `middle_name` varchar(20) DEFAULT null,
+  `last_name` varchar(20) NOT NULL,
+  `e_mail` varchar(30) DEFAULT null,
+  `phone_number` varchar(15) NOT NULL,
+  `document_id` varchar(20) NOT NULL
 );
 
-create table employees(
-id int primary key auto_increment,
-first_name varchar(20),
-middle_name varchar(20),
-last_name varchar(20)
+CREATE TABLE `employees` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(20) NOT NULL,
+  `middle_name` varchar(20) DEFAULT null,
+  `last_name` varchar(20) NOT NULL
 );
 
-create table job_history(
-id int primary key auto_increment,
-emp_id int,
-occupation varchar(20),
-start_date date,
-end_date date,
-foreign key (emp_id) references employees(id)
+CREATE TABLE `job_history` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `emp_id` int NOT NULL unique,
+  `occupation` varchar(20) NOT NULL,
+  `salary` int NOT NULL,
+  `start_date` timestamp NOT NULL,
+  `end_date` timestamp DEFAULT null
 );
 
-create table bill(
-id int primary key auto_increment,
-book_id int,
-emp_id int,
-foreign key (book_id) references booking(id),
-foreign key (emp_id) references employees(id)
+CREATE TABLE `rooms` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `room_number` varchar(10) NOT NULL,
+  `class` varchar(20) NOT NULL,
+  `price_per_day` int NOT NULL,
+  `available` tinyint(1) DEFAULT '1'
 );
+
+ALTER TABLE `bill` ADD FOREIGN KEY (`book_id`) REFERENCES `booking` (`id`);
+
+ALTER TABLE `bill` ADD FOREIGN KEY (`emp_id`) REFERENCES `employees` (`id`);
+
+ALTER TABLE `bill` ADD FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`);
+
+ALTER TABLE `booking` ADD FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`);
+
+ALTER TABLE `job_history` ADD FOREIGN KEY (`emp_id`) REFERENCES `employees` (`id`);
